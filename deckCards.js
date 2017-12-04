96d74ca781c6f95d9b59b4453bede012dcf76e93
Bald Mike <baldmike312@gmail.com>
2017-12-04 09:54:21 -0600

    initial commit

--- deckCards.js
+++ deckCards.js
@@ -31,11 +31,8 @@ class Deck{
 
     shuffle(){
         var m = this.cards.length, t, i;
-        // While there remain elements to shuffle…
         while (m) {
-            // Pick a remaining element…
             i = Math.floor(Math.random() * m--);
-            // And swap it with the current element.
             t = this.cards[m];
             this.cards[m] = this.cards[i];
             this.cards[i] = t;
